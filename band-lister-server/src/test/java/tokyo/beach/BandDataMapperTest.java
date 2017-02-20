cc56e948a0f450fd89f5a0086818ef3c632d25fb
Kyle Gearhart <kgearhart@pivotal.io>
2017-02-20 15:37:51 +0900

    Experiment with setting container name for db url.
    Signed-off-by: Ichizo Umehara <iumehara@pivotal.io>

--- band-lister-server/src/test/java/tokyo/beach/BandDataMapperTest.java
+++ band-lister-server/src/test/java/tokyo/beach/BandDataMapperTest.java
@@ -55,14 +55,14 @@ public class BandDataMapperTest {
 
     private DataSource createDataSource() {
         DataSource dataSource = new DataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/band_lister_test");
+        dataSource.setUrl("jdbc:mysql://mysql-image:3306/band_lister_test");
         dataSource.setUsername("root");
         return dataSource;
     }
 
     private DataSource createLoginDataSource() {
         DataSource dataSource = new DataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/mysql");
+        dataSource.setUrl("jdbc:mysql://mysql-image:3306/mysql");
         dataSource.setUsername("root");
         return dataSource;
     }
