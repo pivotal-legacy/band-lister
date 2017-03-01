e0c84828ee7e0a4f84074ac2beb443fe5f182235
Derek Boire <dboire@pivotal.io>
2017-03-01 11:25:16 +0900

    Adds BandDetail component for single bands
    Signed-off-by: Ichizo Umehara <iumehara@pivotal.io>

---
+++ band-lister-client-redux/app/js/BandDetail.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default class BandDetail extends React.Component {
+  componentDidMount() {
+    this.props.fetchBand()
+  }
+
+  render() {
+    const band = this.props.band
+    return (
+      <ul key={band.id}>
+        <li className='id'>{band.id}</li>
+        <li className='name'>{band.name}</li>
+        <li className='member-count'>{band.memberCount}</li>
+      </ul>
+    )
+  }
+}
+
+BandDetail.propTypes = {
+  band: React.PropTypes.shape({
+    id: React.PropTypes.number,
+    name: React.PropTypes.string,
+    memberCount: React.PropTypes.number
+  }).isRequired,
+  fetchBand: React.PropTypes.func.isRequired
+}
---
+++ band-lister-client-redux/test/unit/BandDetailTest.js
@@ -0,0 +1,28 @@
+import expect from 'expect'
+import { shallow, mount } from 'enzyme'
+import React from 'react'
+
+import BandDetail from '../../app/js/BandDetail'
+
+describe('BandDetail', () => {
+  it('renders current band details', () => {
+    const currentBand = {id: 1, name: 'The Beatles', memberCount: 4}
+
+    const bandDetail = shallow(<BandDetail fetchCurrentBand={()=>{}} band={currentBand}/>)
+
+    const rowWithId = bandDetail.find('ul li .id')
+    const rowWithName = bandDetail.find('ul li .name')
+    const rowWithMemberCount = bandDetail.find('ul li .member-count')
+
+    expect(rowWithId.node.props.children).toBe(1)
+    expect(rowWithName.node.props.children).toBe('The Beatles')
+    expect(rowWithMemberCount.node.props.children).toBe(4)
+  })
+
+  it('fetches band on mount', () => {
+    const fetchBandSpy = expect.createSpy()
+    const bandDetail = mount(<BandDetail fetchBand={fetchBandSpy} band={{}}/>)
+
+    expect(fetchBandSpy).toHaveBeenCalled()
+  })
+})
