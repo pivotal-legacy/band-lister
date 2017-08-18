84a6029cbc8f18fd2a6587e5f02ea62eb3f20c65
Ichizo Umehara <iumehara@pivotal.io>
2017-08-18 11:31:47 +0900

    Adds more bands to seed file

--- band-lister-server/src/main/resources/db/seed.sql
+++ band-lister-server/src/main/resources/db/seed.sql
@@ -2,14 +2,21 @@ INSERT INTO bands(name, member_count)
 VALUES
 ("The Beatles", 4),
 ("Cat Stevens", 1),
+("Counting Crows", 1),
 ("The Eagles", 5),
 ("Gorillaz", 4),
+("Hootie and the Blowfish", 4),
 ("The Monkees", 5),
+("Noel Gallagher's High Flying Birds", 5),
 ("Phish", 5),
+("Pitbull", 1),
 ("Reel Big Fish", 5),
+("Sheryl Crow", 1),
 ("Snoop Dogg", 1),
+("Steppenwolf", 5),
 ("The Turtles", 5),
-("Whitesnake", 5);
+("Whitesnake", 5),
+("The Yardbirds", 5);
 
 INSERT INTO accounts(username, password)
 VALUES
